f3dba462ce5968c50a02ec5d018371632aa5e92e
Ezat <ezat.rafie@gmail.com>
2022-12-08 16:25:10 -0500

    added splash screen

--- app/src/main/java/com/example/trademanager/MainActivity.java
+++ app/src/main/java/com/example/trademanager/MainActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*
+    icons are downloaded from <a target="_blank" href="https://icons8.com/icon/NwAwrEVExFBt/stocks">Stocks</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    images are downloaded from https://www.pexels.com/search/stock%20market/
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
