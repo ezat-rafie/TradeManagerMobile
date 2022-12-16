a1bdd22796ace2139065c46a4a75eb61fb5bd899
Ilshin Ji <ilshin714@gmail.com>
2022-12-16 13:07:16 -0500

    Overall Performance partially done with Asset Allocation

--- app/src/main/java/com/example/trademanager/OverallPerformanceActivity.java
+++ app/src/main/java/com/example/trademanager/OverallPerformanceActivity.java
@@ -7,29 +7,46 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class OverallPerformanceActivity extends AppCompatActivity {
+
+    DBHelper dbhelper = new DBHelper(this);
+    ListView assetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_overall_performance);
+        assetList = (ListView) findViewById(R.id.assetList);
+        double totalPurchase = 0;
 
-        DBHelper dbhelper = new DBHelper(this);
-        Toast.makeText(this, "DB created", Toast.LENGTH_LONG).show();
-        Asset a = new Asset();
-        a.name = "test";
-        Date date = new Date();
-        a.purchaseDate = date;
-        a.entryPrice = 44.33;
-        a.amount = 20;
-        dbhelper.addAsset(a);
+        ArrayList<Asset> assets = dbhelper.getAllAssets();
+        for (Asset ast: assets ) {
+            totalPurchase += ast.amount * ast.entryPrice;
+        }
 
+        ArrayList<String> assetDisplay = setAssetAllocation(assets, totalPurchase);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, R.layout.activity_listview, R.id.textViewAllocation, assetDisplay);
+        assetList.setAdapter(arrayAdapter);
+    }
+
+    ArrayList<String> setAssetAllocation (ArrayList<Asset> assets, double totalPurchase){
+        ArrayList<String> assetDisplay = new ArrayList<>();
+
+        for (Asset ast: assets ) {
+
+            int portion = (int) ((ast.amount * ast.entryPrice) / totalPurchase *100 );
+            assetDisplay.add(ast.name + " at " + ast.purchaseDate + ":    " + portion + "% ");
+        }
+        return assetDisplay;
     }
 
     @Override
