3ef7183451021ef4e22492cf696bb76b32bac713
Ilshin Ji <ilshin714@gmail.com>
2022-12-16 23:47:16 -0500

    added about

--- app/src/main/java/com/example/trademanager/AboutActivity.java
+++ app/src/main/java/com/example/trademanager/AboutActivity.java
@@ -7,14 +7,27 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        textview = (TextView) findViewById(R.id.textAbout);
+
+        textview.setText(
+                "\n" +
+                "Author:"+"\nKeum Ji Kim,  "+ "\nEzatullah Rafie,"  + "\nIlshin Ji" + "\n"+"\n"+
+                "Trade Manager enables investors investing in different markets to have a single dashboard with all their trades listed. On top of that, with Trade Manager users will be able to see their portfolio’s overall performance and make decisions. \n" +
+                "\n" +
+                "Our application retrieves the most up-to-date price of assets and does all the calculations to display the unrealized panel of each trade on a set interval. This will allow users to manage their returns in a more efficient manner and saves time by gathering everything in one place. \n" +
+                "\n");
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
